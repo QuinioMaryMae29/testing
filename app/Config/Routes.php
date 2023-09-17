147133f840d4a16e276d5c1597b56d82df12b874
mary-mae <quiniomarymaea@gmail.com>
2023-09-17 11:30:22 +0800

    view

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,4 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/test', 'Home::test');
+$routes->get('/product', 'ProductController::test');
+$routes->get('/product/(:any)', 'ProductController::product/$1');
--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -6,6 +6,16 @@ use App\Controllers\BaseController;
 
 class ProductController extends BaseController
 {
+    public function product($product)
+    {
+        echo $product;
+    }
+
+    public function test()
+    {
+        return view('products');
+    }
+
     public function index()
     {
         //
